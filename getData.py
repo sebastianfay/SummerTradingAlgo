296f3c45bffba699be12aa01ba537b3b64239266
Sebastian Fay <sebastianwfay@gmail.com>
2018-04-22 22:25:28 -0400

    Wrote intro comments

---
+++ getData.py
@@ -0,0 +1,12 @@
+# Sebastian Fay
+
+# The purpose of this file is to retrieve the current and historical RSI,
+# MACD, and price data of certain stocks and print them in a .txt file so
+# that it can be easily read and analyzed by my c++ programs.
+
+import json
+import requests
+
+apiKey = "LFUM3T5DS62ZKDC0"
+
+# =======================   RSI     
---
+++ niseko.cpp
@@ -0,0 +1,28 @@
+// Sebastian Fay
+
+// -----------------------------------------------------------------------------
+// THIS IS JUST FOR EDUCATIONAL PURPOSES, DO NOT MAKE TRADING DECISIONS BASED ON
+// THIS ALGORITHM
+// -----------------------------------------------------------------------------
+
+// This program ideally will find day-trading opportunities of penny stocks
+// using the RSI movement, MACD crossovers, and the price movement
+
+// Trading Theory:
+// I noticed when looking at the MACD and RSI charts of small-cap stocks that a
+// possible indicator for a rise in market price are often shortly after a bullish
+// MACD crossover and the RSI rising above 30 from an oversold state.
+
+// This strategy uses mean reversion techniques to verify a forming short term
+// trend, or vis versa.
+
+class Niseko{
+public:
+
+public:
+
+};
+
+int main(){
+  return 0;
+}
